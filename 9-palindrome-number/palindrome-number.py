d71e2c8ea8b950720d0c1fe49a4c7f583599e9ca
jethavah042-hash <jethavah042@gmail.com>
2026-01-28 15:29:38 +0530

    Time: 9 ms (62.53%) | Memory: 12.5 MB (16.41%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, x):
+      
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reversed_half = 0
+
+        
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+
+        # For even length numbers: x == reversed_half
+        # For odd length numbers:  x == reversed_half // 10
+        return x == reversed_half or x == reversed_half // 10
+
+        
